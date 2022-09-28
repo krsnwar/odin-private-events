c2e290f0ddf32cd16c22cadae7bffe3d0de44c29
Krisna Wardhana <krisna.wardhana@ukirama.com>
2022-09-28 16:19:32 +0700

    Make form to create event and showing event details

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,5 +3,29 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.all
+    @users = User.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    @event = current_user.events.create(event_params)
+
+    if @event.save
+      redirect_to events_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def event_params
+      params.require(:event).permit(:name, :description, :date)
+    end
 end
