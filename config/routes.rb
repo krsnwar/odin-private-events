e65b41afb5a60e9bbbb3ae5026d05d9ae6b34266
Krisna Wardhana <krisna.wardhana@ukirama.com>
2022-09-28 11:46:45 +0700

    Set up devise and set root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'events/index'
+  devise_for :users
+
+  root "events#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
