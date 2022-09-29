893694a51fd2887f22322b011c82d49902bf7b86
Krisna Wardhana <krisna.wardhana@ukirama.com>
2022-09-29 09:07:24 +0700

    Add association between event and users as an atendee

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :user
+  has_many :event_atendees
+  has_many :atendee, through: :event_atendees, source: :user
 
   validates :name, presence: true
   validates :description, presence: true
---
+++ app/models/event_atendee.rb
@@ -0,0 +1,4 @@
+class EventAtendee < ApplicationRecord
+  belongs_to :user
+  belongs_to :event
+end
